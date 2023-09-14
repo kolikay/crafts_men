379c6e51575ae10ce43a87a125560e835397c245
kolikay <kolikay1989@gmail.com>
2023-09-14 14:52:50 +0100

    completed locations screens

--- lib/screens/location/location_screen2.dart
+++ lib/screens/location/location_screen2.dart
@@ -19,46 +19,48 @@ class _LocationScreen2State extends State<LocationScreen2> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 40.h),
-          child: Column(
-            children: [
-                 SizedBox(
-                height: 15.h,
-              ),
-              SizedBox(
-                height: 124.h,
-                width: 103.w,
-                child: Image.asset('lib/assets/logoTrans.png'),
-              ),
-              SizedBox(
-                height: 55.h,
-              ),
-              NormalText(
-                text: 'You are In',
-                fontWeight: FontWeight.w600,
-                size: 34.sp,
-                color: kMainColor,
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              Text(
-                """Thanks for taking time your time to create an account 
-                with us. Now this is the fun part. Let’s explore the app """,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: kBlackDull,
-                  fontSize: 15.sp,
-                  fontWeight: FontWeight.w400,
+      child: SafeArea(
+        child: Scaffold(
+          body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 40.h),
+            child: Column(
+              children: [
+                   SizedBox(
+                  height: 15.h,
                 ),
-              ),
-              SizedBox(
-                height: 25.h,
-              ),
-              ReuseableButton(text: 'Start', onPressed: () {})
-            ],
+                SizedBox(
+                  height: 124.h,
+                  width: 103.w,
+                  child: Image.asset('lib/assets/logoTrans.png'),
+                ),
+                SizedBox(
+                  height: 55.h,
+                ),
+                NormalText(
+                  text: 'WELCOME!!!',
+                  fontWeight: FontWeight.w600,
+                  size: 34.sp,
+                  color: kMainColor,
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Text(
+                  """Thanks for taking time your time to create an account 
+                  with us.Now this is the fun part. Let’s explore the app """,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: kBlackDull,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  height: 25.h,
+                ),
+                ReuseableButton(text: 'Start', onPressed: () {})
+              ],
+            ),
           ),
         ),
       ),
